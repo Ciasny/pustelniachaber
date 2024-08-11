34c25441da0cee8d377161c3660212033aa77b34
Ciasny <manziolo123@gmail.com>
2024-08-11 13:02:30 +0200

    add pracownia slider

--- src/Components/HomePage/NewLocation.jsx
+++ src/Components/HomePage/NewLocation.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Slider from '../Main/Slider'
-import imgSlider1 from 'src/Images/Flowerbox/img1.jpg'
-import imgSlider2 from 'src/Images/Flowerbox/img2.jpg'
-import imgSlider3 from 'src/Images/Flowerbox/img3.jpg'
+import imgSlider1 from 'src/Images/Pracownia/inside1.jpg'
+import imgSlider2 from 'src/Images/Pracownia/inside2.jpg'
 
 
 const NewLocation = () => {
@@ -26,28 +25,35 @@ const NewLocation = () => {
                     </p>
                 </div>
                 <div className='flex gap-[4rem] mb-4 max-md:flex-col'>
-                    <div className='max-w-md overflow-hidden rounded-2xl'>
-                        <Slider config={{
-                            type: 'loop',
-                            infinite: true,
-                            autoplay: true,
-                            speed: 1000,
-                            interval: 2500,
-                            breakpoints: {
-                                768: {
-                                    padding: '3rem',
-                                    gap: '1rem'
+                    <div>
+                        <p className=' text-2xl font-semibold text-center mb-2'>-Zdjęcia Pracowni-</p>
+                        <div className='max-w-md overflow-hidden rounded-2xl'>
+
+                            <Slider config={{
+                                type: 'loop',
+                                infinite: true,
+
+                                breakpoints: {
+                                    768: {
+                                        padding: '3rem',
+                                        gap: '1rem'
+                                    }
                                 }
-                            }
-                        }} >
-                            <img src={imgSlider1} alt='Kwiaty' className='w-full object-cover aspect-square m-auto' />
-                            <img src={imgSlider2} alt='Kwiaty' className='w-full object-cover aspect-square m-auto' />
-                            <img src={imgSlider3} alt='Kwiaty' className='w-full object-cover aspect-square m-auto' />
-                        </Slider>
+                            }} >
+                                <img src={imgSlider1} alt='Pracownia - Środek' className='w-full object-cover aspect-square m-auto' />
+                                <img src={imgSlider2} alt='Pracownia - Środek' className='w-full object-cover aspect-square m-auto' />
+                                {/* <img src={imgSlider3} alt='Kwiaty' className='w-full object-cover aspect-square m-auto' /> */}
+                            </Slider>
+                        </div>
                     </div>
-                    <iframe className='w-full rounded-2xl'
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d226.5156754378644!2d19.238327245496347!3d50.03511139539742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716950eb0310541%3A0x9d28298a168d31e8!2zSsSZZHJ6ZWphIMWabmlhZGVja2llZ28gMzMsIDMyLTYwMCBPxZt3acSZY2lt!5e0!3m2!1spl!2spl!4v1723221145855!5m2!1spl!2spl"
-                        loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+                    <div>
+                        <p className=' text-2xl font-semibold text-center mb-2'>-Lokalizacja Pracowni-</p>
+
+                        <iframe className='w-full rounded-2xl'
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d226.5156754378644!2d19.238327245496347!3d50.03511139539742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716950eb0310541%3A0x9d28298a168d31e8!2zSsSZZHJ6ZWphIMWabmlhZGVja2llZ28gMzMsIDMyLTYwMCBPxZt3acSZY2lt!5e0!3m2!1spl!2spl!4v1723221145855!5m2!1spl!2spl"
+                            loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
 
             </div>
